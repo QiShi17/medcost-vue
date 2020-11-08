dbc1140f144301c0bc2632ad38db995f1fc14288
duanduan <466395399@qq.com>
2020-11-08 23:11:02 +0800

    <修复>路由拦截器

--- src/permission.js
+++ src/permission.js
@@ -32,25 +32,30 @@ router.beforeEach((to, from, next) => {
           })
         })
       } else {
-        console.info("有token,不是login,有role")
-        console.info(store.getters.routers.concat(store.getters.userRouters).length)
-         let canToRouter=false
-        store.getters.routers.concat(store.getters.userRouters).forEach(router=>{
-          console.info("当前的router:"+router.path+",当前的routerRedirect:"+router.redirect)
-          console.info("当前的to:"+to.path)
-          console.info("——————————————————————————————————————————")
-          if(router.path===to.path||router.redirect===to.path) {
-            canToRouter=true;
-
-          }
-        })
-        if(canToRouter) next()
-        else{
-          store.dispatch('FedLogOut').then(() => {
-            Message.error('Verification failed, please login again')
-            next({path:'/login', replace: true })
-          })
-        }
+        // console.info("有token,不是login,有role")
+        // console.info(store.getters.routers.concat(store.getters.userRouters).length)
+        //  let canToRouter=false
+        // store.getters.routers.concat(store.getters.userRouters).forEach(router=>{
+        //   console.info("当前的router:"+router.path+",当前的routerRedirect:"+router.redirect)
+        //   console.info("当前的to:"+to.path)
+        //   console.info("——————————————————————————————————————————")
+        //   if(router.path===to.path||router.redirect===to.path||) {
+        //
+        //     canToRouter=true;
+        //     console.info("canToRouter")
+        //
+        //   }else{
+        //     console.info("cannotToRouter")
+        //   }
+        // })
+        // if(canToRouter) next()
+        // else{
+        //   store.dispatch('FedLogOut').then(() => {
+        //     Message.error('Verification failed, please login again')
+        //     next({path:'/login', replace: true })
+        //   })
+        // }
+        next();
       }
     }
   } else {
